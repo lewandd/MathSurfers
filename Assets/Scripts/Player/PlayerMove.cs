9c05ab29915cf84dd513fa7729ecfc7c0520dc2e
lewandd <lewandd9@gmail.com>
2023-01-13 08:33:51 +0100

    Ogarnięcie obsługi ruchu

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -18,6 +18,12 @@ public class PlayerMove : MonoBehaviour
     private float rightRoad = 2.0f;
     private int actualRoad = 1;
 
+    private bool turningLeft = false;
+    private bool turningRight = false;
+
+    float rot = 0.0f;
+    float acc = 0.1f;
+
     void Start()
     {
         actualMoveSpeed = baseMoveSpeed;
@@ -48,74 +54,105 @@ public class PlayerMove : MonoBehaviour
             ui.GetComponent<UI>().setGameOver();
         }
 
+        handleInput();
 
-        if (!gameOver)
-        {
+        if (!gameOver) {
             transform.Translate(Vector3.forward * Time.deltaTime * actualMoveSpeed, Space.World);
+            
+            moveLane();
+        }
+        
+    }
 
-            if (!(movingLeft || movingRight))
-            {
-                
-
-                if (Input.GetKey(KeyCode.A))
-                {
+    private void moveLane() {
+        if (!(movingLeft || movingRight)) {
+                if (turningLeft) {
                     if (this.gameObject.transform.position.x > LevelBoundary.leftSide)
                         movingLeft = true;
-                            //transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed, Space.World);
                 }
-                else if (Input.GetKey(KeyCode.D))
-                {
-
+                else if (turningRight) {
                     if (this.gameObject.transform.position.x < LevelBoundary.rightSide)
                         movingRight = true;
-                    //transform.Translate(-1 * Vector3.left * Time.deltaTime * leftRightSpeed, Space.World);
                 }
             }
-            if (movingLeft)
-            {
-                //if (this.gameObject.transform.position.x > LevelBoundary.leftSide)
-                //transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed, Space.World);
-                if (actualRoad == 0)
-                {
+            if (movingLeft) {
+                if (actualRoad == 0) {
                     movingLeft = false;
                 }
-                else if (actualRoad == 1 && this.gameObject.transform.position.x <= leftRoad)
-                {
+                else if (actualRoad == 1 && this.gameObject.transform.position.x <= leftRoad) {
                     movingLeft = false;
                     actualRoad = 0;
                 }
-                else if (actualRoad == 2 && this.gameObject.transform.position.x <= midRoad)
-                {
+                else if (actualRoad == 2 && this.gameObject.transform.position.x <= midRoad) {
                     movingLeft = false;
                     actualRoad = 1;
                 }
-                else
-                {
+                else {
                     transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed, Space.World);
                 }
             }
-            else if (movingRight)
-            {
-                if (actualRoad == 0 && this.gameObject.transform.position.x >= midRoad)
-                {
+            else if (movingRight) {
+                if (actualRoad == 0 && this.gameObject.transform.position.x >= midRoad) {
                     movingRight = false;
                     actualRoad = 1;
                 }
-                else if (actualRoad == 1 && this.gameObject.transform.position.x >= rightRoad)
-                {
+                else if (actualRoad == 1 && this.gameObject.transform.position.x >= rightRoad) {
                     movingRight = false;
                     actualRoad = 2;
                 }
-                else if (actualRoad == 2)
-                {
+                else if (actualRoad == 2) {
                     movingRight = false;
                 }
-                else
-                {
+                else {
                     transform.Translate(-1 * Vector3.left * Time.deltaTime * leftRightSpeed, Space.World);
                 }
             }
+    }
+
+    private void moveFree() {
+        if (turningLeft)
+            if (this.gameObject.transform.position.x > LevelBoundary.leftSide)
+                transform.Translate(-1 * rot * Vector3.left * Time.deltaTime * leftRightSpeed, Space.World);
+
+        if (turningRight)
+            if (this.gameObject.transform.position.x < LevelBoundary.rightSide)
+                    transform.Translate(-1 * rot * Vector3.left * Time.deltaTime * leftRightSpeed, Space.World);
+    }
+
+    private void handleInput() {
+        rot = Input.GetAxis("Horizontal") * Time.deltaTime * 300.0f;
+
+        if (rot < -acc) {
+            turningLeft = true;
+            turningRight = false;
+
+            
         }
-        
+        else if(rot > acc) {
+            turningLeft = false;
+            turningRight = true;
+        }
+        else {
+            turningLeft = false;
+            turningRight = false;
+        }
+    }
+
+    private void handleInputKey() {
+        if (Input.GetKey(KeyCode.A)) {
+            rot = -1.0f;
+            turningLeft = true;
+            turningRight = false;
+        }
+        else if (Input.GetKey(KeyCode.D)) {
+            rot = 1.0f;
+            turningLeft = false;
+            turningRight = true;
+        }
+        else {
+            rot = 0.0f;
+            turningLeft = false;
+            turningRight = false;
+        }
     }
 }
